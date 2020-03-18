e73fa982d0aff11ee21599bdf974aaad2fd425e2
Shahriyar Mirzai <johndoe@example.com>
2020-03-18 16:38:40 +0400

    Js update

--- javascript.js
+++ javascript.js
@@ -10,16 +10,29 @@ sum.addEventListener('click',function(){
     console.clear();
     let balance = 0;
     time = Math.floor(time/30);
-    if(cv>0 && depo>=0 && rait >0 && time>0){
+    if(cv>0 && depo>=0 &&  rait<100  &&  rait > 0 && time>0){
         balance = (cv*Math.pow(1+(rait/100)/12,time)) + depo*findPower(time,rait);
-        alert(balance);
+        alert(balance.toFixed(2));
     }else{
-        result.innerHTML = "Error";
+        check(cv,depo,rait,time)
         console.log(NaN);
     }
     
 })
-
+function check(cv,depo,rait,time){
+    if(cv < 0 || cv ==""){ 
+        result.innerHTML += `Начальная сумма не может быть пустой или ниже нуля <br>`;
+    }
+    if(depo < 0 ){ 
+        result.innerHTML += `Сумма ежемесячного вклада не может быть пустой или ниже нуля <br>`;
+    }
+    if(rait <= 0 || rait >100){ 
+        result.innerHTML += `Процентная ставка не может быть пустой или ниже нуля или выше нуля <br>`;
+    }
+    if(time <= 0){ 
+        result.innerHTML += `Срок вклада не может быть пустой или ниже нуля <br>`;
+    }
+}
 function findPower(t,r){
     let salPower = 0;
     for(i=t;i>0;i--){
