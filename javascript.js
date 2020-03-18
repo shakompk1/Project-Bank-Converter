5763d70eed68ff72a0d5cf928f95343aa6dbdd2a
Shahriyar Mirzai <johndoe@example.com>
2020-03-18 15:38:20 +0400

    Js update

--- javascript.js
+++ javascript.js
@@ -6,15 +6,16 @@ sum.addEventListener('click',function(){
     let depo =+(document.getElementById('depo').value);
     let rait =+(document.getElementById('rait').value);
     let time =+(document.getElementById('time').value);
-    result.innerHTML = ""
+    result.innerHTML = "";
+    console.clear();
     let balance = 0;
-    time = Math.floor(time/30)
+    time = Math.floor(time/30);
     if(cv>0 && depo>=0 && rait >0 && time>0){
-        balance = (cv*Math.pow(1+(rait/100)/12,time)) + depo*findPower(time,rait)
-        alert(balance)
+        balance = (cv*Math.pow(1+(rait/100)/12,time)) + depo*findPower(time,rait);
+        alert(balance);
     }else{
-        result.innerHTML = "Error"
-        console.log(NaN)
+        result.innerHTML = "Error";
+        console.log(NaN);
     }
     
 })
@@ -22,7 +23,7 @@ sum.addEventListener('click',function(){
 function findPower(t,r){
     let salPower = 0;
     for(i=t;i>0;i--){
-      salPower += Math.pow(1+(r/100)/12,i)
+      salPower += Math.pow(1+(r/100)/12,i);
     }
-    return salPower
+    return salPower;
 }
